b19ad6109c17bdb8bc63f339d81a5d0493b8d16e
Hareesh Naik <hareeshnaik836@gmail.com>
2025-04-12 12:16:45 +0530

    new updates

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -2,26 +2,43 @@ import React, { useRef } from 'react';
 import { motion, useInView } from 'framer-motion';
 import './Skills.css';
 
+
+import cppImg from '../../assets/skills/c-.png';
+import jsImg from '../../assets/skills/js.png';
+import htmlImg from '../../assets/skills/html-5.png';
+import cssImg from '../../assets/skills/css-3.png';
+import tailwindImg from '../../assets/skills/tailwind.png';
+import bootstrapImg from '../../assets/skills/bootstrap.png';
+import gitImg from '../../assets/skills/git.png';
+import reactImg from '../../assets/skills/react.png';
+import pythonImg from '../../assets/skills/python.png';
+import mysqlImg from '../../assets/skills/mysql.png';
+import nodeImg from '../../assets/skills/nodejs.png';
+import mongoImg from '../../assets/skills/mongodb.png';
+import officeImg from '../../assets/skills/office.png';
+import awsImg from '../../assets/skills/aws.png';
+import expressImg from '../../assets/skills/express.png';
+
 const Skills = () => {
   const containerRef = useRef(null);
   const isInView = useInView(containerRef);
 
   const allSkills = [
-    { name: 'C++', image: '../src/assets/skills/c-.png' },
-    { name: 'JavaScript', image: '../src/assets/skills/js.png' },
-    { name: 'HTML', image: '../src/assets/skills/html-5.png' },
-    { name: 'CSS', image: '../src/assets/skills/css-3.png' },
-    { name: 'Tailwind', image: '../src/assets/skills/tailwind.png' },
-    { name: 'Bootstrap', image: '../src/assets/skills/bootstrap.png' },
-    { name: 'Git', image: '../src/assets/skills/git.png' },
-    { name: 'React', image: '../src/assets/skills/react.png' },
-    { name: 'Python', image: '../src/assets/skills/python.png' },
-    { name: 'MySQL', image: '../src/assets/skills/mysql.png' },
-    { name: 'Node.js', image: '../src/assets/skills/nodejs.png' },
-    { name: 'MongoDB', image: '../src/assets/skills/mongodb.png' },
-    { name: 'Office', image: '../src/assets/skills/office.png' },
-    { name: 'AWS', image: '../src/assets/skills/aws.png' },
-    { name: 'Express', image: '../src/assets/skills/express.png' }
+    { name: 'C++', image: cppImg },
+    { name: 'JavaScript', image: jsImg },
+    { name: 'HTML', image: htmlImg},
+    { name: 'CSS', image: cssImg },
+    { name: 'Tailwind', image:tailwindImg},
+    { name: 'Bootstrap', image: bootstrapImg},
+    { name: 'Git', image:  gitImg },
+    { name: 'React', image: reactImg },
+    { name: 'Python', image: pythonImg },
+    { name: 'MySQL', image: mysqlImg },
+    { name: 'Node.js', image: nodeImg},
+    { name: 'MongoDB', image: mongoImg},
+    { name: 'Office', image: officeImg},
+    { name: 'AWS', image: awsImg  },
+    { name: 'Express', image: expressImg  }
 
   ];
 
